8a302661b6a3c9ecf8ac12244ebd197e59d4e4bb
Mohamed Abdelwahab <moh.elwah@gmail.com>
2023-03-05 00:06:28 +0200

    change

--- chatgpt_test/app.py
+++ chatgpt_test/app.py
@@ -1,5 +1,4 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
@@ -8,22 +7,18 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 MODEL = "gpt-3.5-turbo"
 
-messages = [
-    {"role": "user", "content": "write print statements in python"},
-]
-
-
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         message = request.form["message"]
         response = openai.ChatCompletion.create(
-    model=MODEL,
-    messages=generate_message(message),
-    temperature=0,
-)
-        return redirect(url_for("index", result=response['choices'][0]['message']['content']))
+                        model=MODEL,
+                        messages=generate_message(message),
+                        temperature=0,
+                    )
+        result = response['choices'][0]['message']['content']
+        return redirect(url_for("index", result=result))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
